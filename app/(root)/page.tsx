0247900ce45ddd66d3453a0885c3c17221e9254c
Aditya Ghosh <adityaghosh2002@gmail.com>
2024-06-08 17:09:47 +0800

    homepage card and sidenav for lg screen

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+
 
 const Home = () => {
+  const loggedIn = {firstName: 'Aditya'};
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and Manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          ></TotalBalanceBox>
+        </header>
+      </div>
+    </section>
   )
 }
 
